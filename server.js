29b797cf19c45572a40cfdd3cf46cd9d594a9dd2
Amit_Nextview <amitnextview@gmail.com>
2025-11-21 12:48:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,12 +26,27 @@ console.log(CLIENT_URL);
 
 app.set("trust proxy", 1);
 
+// 🔥 Global Preflight FIX
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL || "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");  // FIX
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);  // IMPORTANT FOR RENDER
+  }
+
+  next();
+});
+
+
 app.use(cors({
   origin: [
     process.env.CLIENT_URL,
     "http://localhost:3000"
   ],
-   methods: ["GET", "POST", "PUT", "DELETE"],
+   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,                // allow cookies/auth headers
 }));
 
